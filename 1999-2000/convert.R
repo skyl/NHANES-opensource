665b94eb53dfc61a994290cb5064e87fd2367653
Skylar Saveland <skylar.saveland@jpmchase.com>
2015-02-05 14:30:20 -0800

    missing DRXIFF and some maintenance

--- 1999-2000/convert.R
+++ 1999-2000/convert.R
@@ -1,20 +1,7 @@
 library(Hmisc)
 
-moveAndDumpRelative <- function(wd, td) {
-  setwd(wd)
-  for (filename in list.files(pattern="*.xpt")) {
-    print(filename)
-    xpt <- sasxport.get(filename)
-    fl <- strsplit(filename, "\\.")
-    target <- paste(td, fl[[1]][1], ".csv", sep="")
-    print(target)
-    write.csv(xpt, file=target)
-  }
+convertXPTtoCSV <- function(from_path, to_path) {
+    xpt <- sasxport.get(from_path)
+    write.csv(xpt, file=to_path)
 }
-
-moveAndDumpRelative("xpt", "../csv/")
-moveAndDumpRelative("diet", "../../csv/diet/")
-moveAndDumpRelative("../exam/", "../../csv/exam/")
-moveAndDumpRelative("../lab/", "../../csv/lab/")
-moveAndDumpRelative("../quest/", "../../csv/quest/")
 
